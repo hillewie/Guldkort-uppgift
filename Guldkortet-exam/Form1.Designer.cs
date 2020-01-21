5941447ca2728a87abfc4578b33bbae90f3c1138
hillewie <55441120+hillewie@users.noreply.github.com>
2020-01-21 14:07:52 +0100

    dasda
    asdsa

--- Guldkortet-exam/Form1.Designer.cs
+++ Guldkortet-exam/Form1.Designer.cs
@@ -28,86 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.button3 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(26, 253);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(217, 65);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "Stop server";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.Button1_Click);
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(26, 336);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(217, 63);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "startserver";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.Button2_Click);
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Location = new System.Drawing.Point(306, 137);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(251, 262);
-			this.pictureBox1.TabIndex = 2;
-			this.pictureBox1.TabStop = false;
-			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
-			// 
-			// richTextBox1
-			// 
-			this.richTextBox1.Location = new System.Drawing.Point(574, 137);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(186, 262);
-			this.richTextBox1.TabIndex = 3;
-			this.richTextBox1.Text = "";
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(40, 28);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(156, 46);
-			this.button3.TabIndex = 4;
-			this.button3.Text = "Skicka data ";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(252, 44);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(261, 22);
-			this.textBox1.TabIndex = 5;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.richTextBox1);
-			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(306, 137);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 262);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
 
         }
 
@@ -116,9 +84,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.RichTextBox richTextBox1;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.TextBox textBox1;
-	}
+    }
 }
 
--- Guldkortet-exam/Form1.cs
+++ Guldkortet-exam/Form1.cs
@@ -14,61 +14,70 @@ namespace Guldkortet_exam
 {
     public partial class Form1 : Form
     {
-        private readonly FileLoader kortLoader;
+        private readonly KortLoader kortLoader;
         private readonly TcpClient client;
-		private readonly Server server;
-		
-		public Form1()
+
+        public Form1()
         {
             InitializeComponent();
-            kortLoader = new FileLoader();
+            kortLoader = new KortLoader();
             client = new TcpClient();
-			server = new Server(kortLoader);
-			richTextBox1.DataBindings.Add("Text", server, "Log", true, DataSourceUpdateMode.OnPropertyChanged);
-			textBox1.Text = "A2986708-K242872563";
-			button1.Enabled = false;
-			button3.Enabled = false;
-		}
+
+            string Kund = "";
+            string Kort = "";
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
-			
-			
-		}
+        }
 
-        private async void Button1_Click(object sender, EventArgs e)
-        {
-			try
-			{
-				server.Stop();
-				button2.Enabled = true;
-				button1.Enabled = false;
-				button3.Enabled = false;
-			}
-			catch (Exception error)
-			{
-				MessageBox.Show(error.Message);
-			}
-		}
-
-        private async  void Button2_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             try
             {
-				if (!server.stop)
-				{
-					server.Log = "server is starting";
-					button2.Enabled = false;
-					button1.Enabled = true;
-					button3.Enabled = true;
-					await server.StartServer();
-
-				}
+                if (client.Connected)
+                {
+                    NetworkStream tråden = client.GetStream();
+                    foreach ()
+                    {
+                        byte[] bytesToSend = Encoding.Unicode.GetBytes(Kort.ToString()); //  Programmet kan skicka markerade böckers ToString till Centraldator med tostring
+                        await tråden.WriteAsync(bytesToSend, 0, bytesToSend.Length); // meddelandar servern vad vi ska skicka och hur stort  
+                    }
+                }
             }
             catch (Exception error)
             {
-                
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!client.Connected)
+                {
+                    // Startar uppkoppling med servern och låser up button1, knappen blir grön
+                    await client.ConnectAsync("127.0.0.1", 12345);
+                    // byter färg och text
+                    this.button2.BackColor = Color.Green;
+                    this.button2.Text = "You are connected!";
+
+                    button1.Enabled = true;
+                }
+                else
+                {
+                    // clienten är kopplad och kan avsluta uppkoppligen och låser button1, knappen byter färg till vit 
+                    client.Close();
+                    this.button2.BackColor = Color.White;
+                    this.button2.Text = "Connect to Server";
+                    button1.Enabled = false;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
             }
         }
 
@@ -76,34 +85,149 @@ namespace Guldkortet_exam
         {
 
         }
+    }
+    public class KortLoader
+    {
+        public List<Kort> kortLista = new List<Kort>();
+        public List<Kund> kundLista = new List<Kund>();
 
-		private async void button3_Click(object sender, EventArgs e)
-		{
-			button3.Enabled = false;
-			var client = new TcpClient();
-			
+        public KortLoader()
+        {
+            List<string> kundLista = new List<string>();
+            if (File.Exists("kundlista.txt"))
+            {
+                StreamReader kundReader = new StreamReader("kundlista.txt", Encoding.Default, true);
 
-				await client.ConnectAsync(server.IPNumeber, server.Port);
-			using (NetworkStream networkStream = client.GetStream())
-			{
-				networkStream.ReadTimeout = 2000;
+                string kund = "";
+                while ((kund = kundReader.ReadLine()) != null)
+                {
+                    kundLista.Add(kund);
+                }
+                foreach (string kund in kundLista)
+                {
+                    string[] kundVektor = kund.Split(new string[] { "###" }, StringSplitOptions.None);
+                    kundLista.Add(new kund(kundVektor[0], kundVektor[1], kundVektor[2]));
+                }
+            }
 
-				using (var writer = new StreamWriter(networkStream))
-				{
-					using (var reader = new StreamReader(networkStream, Encoding.Unicode))
-					{
+            List<string> kortLista = new List<string>();
+            if (File.Exists("kortlista.txt"))
+            {
+                StreamReader kortReader = new StreamReader("kortlista.txt", Encoding.Default, true);
 
-						byte[] bytes = Encoding.Unicode.GetBytes(textBox1.Text);
-						await networkStream.WriteAsync(bytes, 0, bytes.Length);
+                string kort = "";
+                while ((kort = kortReader.ReadLine()) != null)
+                {
+                    kortLista.Add(kort);
+                }
 
-						MessageBox.Show(await reader.ReadToEndAsync());
-					}
-				}
-			}
-			button3.Enabled = true;
-		}
+                foreach (string sparatkort in kortLista)
+                {
+                    string[] kortVektor = sparatKort.Split(new string[] { "###" }, StringSplitOptions.None);
+                    string Dunderkatt = kortVektor[0];
+                    string Kristallhäst = kortVektor[1];
+                    string Överpanda = kortVektor[2];
+                    string Eldtomat = kortVektor[3];
 
-		
-	}
-   
+                    Kort kort;
+                    switch (typ)
+                    {
+                        case "Dunderkatt":
+                            kort = new Dunderkatt(kortVektor[0]);
+                            break;
+
+                        case "Kristallhäst":
+                            kort = new Kristallhäst(kortVektor[1]);
+                            break;
+
+                        case "Överpanda":
+                            kort = new Överpanda(kortVektor[2]);
+                            break;
+
+                        case "Eldtomat":
+                            kort = new Eldtomat(kortVektor[3]);
+                        default:
+                    }
+                    kortLista.Add(Kort);
+                }
+            }
+        }
+        public class Kort
+        {
+            public string kortNummer;
+            public string kortTyp;
+
+            public Kort(string inDatakortNummer, string inDatakortTyp)
+            {
+                kortNummer = inDatakortNummer;
+                kortTyp = inDatakortTyp;
+            }
+        }
+        class Dunderkatt : Kort
+        {
+            public Dunderkatt(string inDatakortNummer, string inDatakortTyp) :
+            base(inDatakortNummer, inDatakortTyp)
+            {
+                inDatakortTyp = "Dunderkatt";
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+        class Kristallhäst : Kort
+        {
+            public Kristallhäst(string inDatakortNummer, string inDatakortTyp) :
+            base(inDatakortNummer, inDatakortTyp)
+            {
+                inDatakortTyp = "Kristallhäst";
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+        class Överpanda : Kort
+        {
+            public Överpanda(string inDatakortNummer, string inDatakortTyp) :
+            base(inDatakortNummer, inDatakortTyp)
+            {
+                inDatakortTyp = "Överpanda";
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+        class Eldtomat : Kort
+        {
+            public Eldtomat(string inDatakortNummer, string inDatakortTyp) :
+            base(inDatakortNummer, inDatakortTyp)
+            {
+                inDatakortTyp = "Eldtomat";
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+
+        public class Kund
+        {
+            public string kundNummer;
+            public string kundNamn;
+            public string kundStad;
+
+            public Kund(string inDatakundNummer, string inDatakundNamn, string inDatakundStad)
+            {
+                kundNummer = inDatakundNummer;
+                kundNamn = inDatakundNamn;
+                kundStad = inDatakundStad;
+            }
+            public override string ToString()
+            {
+                return " Grattis " + kundNamn + " Du har vunnit " + kortTyp + " Det finns att hämta i din lokala spelbutik i " + kundStad;
+            }
+        }
+    }
 }
