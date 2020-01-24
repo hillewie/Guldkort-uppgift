63192acc8af2b713868e4666d4f603de3246e82a
hillewie <55441120+hillewie@users.noreply.github.com>
2020-01-24 01:00:07 +0100

    Revert "kod"
    This reverts commit 41f5455049287549ca9aa36f5a9deacecf335c28.

--- Guldkortet-exam/Form1.cs
+++ Guldkortet-exam/Form1.cs
@@ -15,19 +15,21 @@ namespace Guldkortet_exam
     public partial class Form1 : Form
     {
         private readonly FileLoader FileLoader; //kort och kundklass samt kundlista och kortlista
+        private readonly TcpClient client; // den här behövs ej  bara för test 
         private readonly Server server; //Här är servern
 		public Form1()
         {
             InitializeComponent();
             FileLoader = new FileLoader();
-			server = new Server(FileLoader, richTextBox1);
+            client = new TcpClient();
+			server = Server(kortLoader, richTextBox1);
 			button1.Enabled = false;
 		}
         private void Form1_Load(object sender, EventArgs e)
         {
 		
 		}
-        private  void Button1_Click(object sender, EventArgs e)
+        private async void Button1_Click(object sender, EventArgs e)
         {
 			try
 			{
@@ -54,7 +56,7 @@ namespace Guldkortet_exam
             }
             catch (Exception error)
             {
-                MessageBox.Show("server closed"); //felmeddelande
+                MessageBox.Show(error.Message); //felmeddelande
             }
         }
     }
